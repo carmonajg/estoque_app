f828b0cc6d83309335f03921bd5b452af4b08b7b
Joice Gabrielly Carmona <joice.carmona@estudante.ifms.edu.br>
2021-09-28 17:15:40 -0300

    primeiro home quase concluído

---
+++ src/telas/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Image, View, Text} from 'react-native';
+
+import icon from '../../assets/icon.png';
+
+export default function Home() {
+    return  <View style={estilos.container}>
+        <Image source={icon} style={estilos.icon}/>
+            </View>
+};
+
+export function BemVindo() {
+    
+    return <View style={estilos.fora}>
+        <Text style={estilos.texto}> Bem-Vindo </Text>
+    </View>
+};
+
+const estilos = StyleSheet.create({
+    icon: {
+        height: 315,
+        width: 315,
+    },
+    container: {
+        flexDirection: 'column',
+        height: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    fora: {
+        backgroundColor: "#ffa500",
+        width: 130,
+        height: 50,
+        borderRadius: 20,
+        left: "33%"
+},
+    texto:{
+        color: "#FFF",
+        fontSize: 17,
+        textAlign: 'center',
+        marginTop: 15
+    }
+
+});
+
---
+++ src/telas/Home2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeBemVindo() {
+    return  <View>
+        <Text> Oi Carmona</Text>
+            </View>
+}
+
+export default HomeBemVindo;
